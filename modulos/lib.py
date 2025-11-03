518657ffea60106c642e87e39f71057b9314215e
Manuel Miglioranza <mmiglioranza22@gmail.com>
2025-11-03 15:53:45 -0300

    Agregar manejo de errores a carga de paises

--- modulos/lib.py
+++ modulos/lib.py
@@ -1,20 +1,20 @@
 import csv
 
-
-
 def cargar_paises():
 	lista = []
-	with open('dataset_base.csv', 'r', newline='') as archivo:
-		lector = csv.reader(archivo)
-		for fila in lector:
-			# Se omite la primer fila que es la que contiene el nombre de las columnas
-			if fila[0] == "nombre":
-				continue
-			else:
-				lista.append({"PAIS": fila[0], "POBLACION": int(fila[1]), "SUPERFICIE": int(fila[2]), "CONTINENTE": fila[3]})
-	
-	return lista[1:]
+	try:
+		with open('dataset_base.csv', 'r', newline='') as archivo:
+			lector = csv.reader(archivo)
+			for fila in lector:
+				# Se omite la primer fila que es la que contiene el nombre de las columnas
+				if fila[0] == "nombre":
+					continue
+				else:
+					lista.append({"PAIS": fila[0], "POBLACION": int(fila[1]), "SUPERFICIE": int(fila[2]), "CONTINENTE": fila[3]})
+		
+		return lista[1:]
+	except Exception as e:
+		print("Error en carga de archivo. Verifique que el formato del archivo CSV con países sea correcto")
 
 
-lista_paises = cargar_paises()
 
